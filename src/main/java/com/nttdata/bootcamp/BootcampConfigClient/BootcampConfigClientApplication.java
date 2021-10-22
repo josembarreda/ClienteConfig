6a0572f34218d294e629dd58031d79992cf045ee
josembarreda <josemanuelbarreda11@gmail.com>
2021-10-22 10:21:23 +0200

    first commit

---
+++ src/main/java/com/nttdata/bootcamp/BootcampConfigClient/BootcampConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.BootcampConfigClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/BootcampConfigClient/BootcampConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.BootcampConfigClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
